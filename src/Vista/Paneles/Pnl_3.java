3bfa45d011bbaae09fb6eecaefcaf39d21219341
AaronZN07 <aaronzn80@gmail.com>
2021-11-25 19:50:16 -0600

    commit

--- src/Vista/Paneles/Pnl_3.java
+++ src/Vista/Paneles/Pnl_3.java
@@ -17,28 +17,16 @@ public class Pnl_3 extends javax.swing.JPanel {
         jPanel1 = new javax.swing.JPanel();
         jLabel5 = new javax.swing.JLabel();
         txtProveedorReal = new javax.swing.JTextField();
-        jLabel6 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         txtCosto = new javax.swing.JTextField();
         txtMontoOfertado = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         txtNumeroContrato = new javax.swing.JTextField();
-        jLabel8 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        txtBanco1 = new javax.swing.JTextField();
         txtObjeto = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
-        txtCuentas1 = new javax.swing.JTextField();
         jLabel10 = new javax.swing.JLabel();
         txtCedula = new javax.swing.JTextField();
-        jLabel11 = new javax.swing.JLabel();
-        jLabel12 = new javax.swing.JLabel();
-        txtBanco3 = new javax.swing.JTextField();
-        txtCuentas3 = new javax.swing.JTextField();
-        jLabel13 = new javax.swing.JLabel();
-        jLabel14 = new javax.swing.JLabel();
-        txtBanco2 = new javax.swing.JTextField();
-        txtCuentas2 = new javax.swing.JTextField();
         jLabel15 = new javax.swing.JLabel();
         txtProveedorFan = new javax.swing.JTextField();
         btnInsertarDemanda = new javax.swing.JButton();
@@ -51,6 +39,9 @@ public class Pnl_3 extends javax.swing.JPanel {
         scpObservacionesDemanda = new javax.swing.JScrollPane();
         txaObservacionesDemanda = new javax.swing.JTextArea();
         jLabel43 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable2 = new javax.swing.JTable();
+        jLabel7 = new javax.swing.JLabel();
 
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
@@ -82,11 +73,8 @@ public class Pnl_3 extends javax.swing.JPanel {
         });
         jPanel1.add(txtProveedorReal, new org.netbeans.lib.awtextra.AbsoluteConstraints(135, 246, 190, -1));
 
-        jLabel6.setText("Banco 1");
-        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 80, -1, -1));
-
-        jLabel1.setText("Número contratación");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(15, 39, -1, -1));
+        jLabel1.setText("Bancos y cuentas");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 30, -1, -1));
         jPanel1.add(txtCosto, new org.netbeans.lib.awtextra.AbsoluteConstraints(135, 204, 190, -1));
         jPanel1.add(txtMontoOfertado, new org.netbeans.lib.awtextra.AbsoluteConstraints(135, 120, 190, -1));
 
@@ -94,55 +82,16 @@ public class Pnl_3 extends javax.swing.JPanel {
         jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(15, 120, -1, -1));
         jPanel1.add(txtNumeroContrato, new org.netbeans.lib.awtextra.AbsoluteConstraints(135, 36, 190, -1));
 
-        jLabel8.setText("Cuenta 1");
-        jPanel1.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 40, -1, -1));
-
         jLabel3.setText("Objeto");
         jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(15, 81, -1, -1));
-
-        txtBanco1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtBanco1ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(txtBanco1, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 80, 190, -1));
         jPanel1.add(txtObjeto, new org.netbeans.lib.awtextra.AbsoluteConstraints(135, 78, 190, -1));
 
         jLabel4.setText("Costo");
         jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(15, 207, -1, -1));
-        jPanel1.add(txtCuentas1, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 40, 190, -1));
 
         jLabel10.setText("Cedula juridica");
         jPanel1.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(15, 160, -1, -1));
         jPanel1.add(txtCedula, new org.netbeans.lib.awtextra.AbsoluteConstraints(135, 162, 190, -1));
-
-        jLabel11.setText("Banco 3");
-        jPanel1.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 250, -1, -1));
-
-        jLabel12.setText("Cuenta 3");
-        jPanel1.add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 210, -1, -1));
-
-        txtBanco3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtBanco3ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(txtBanco3, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 250, 190, -1));
-        jPanel1.add(txtCuentas3, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 200, 190, -1));
-
-        jLabel13.setText("Banco 2");
-        jPanel1.add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 160, -1, -1));
-
-        jLabel14.setText("Cuenta 2");
-        jPanel1.add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 120, -1, -1));
-
-        txtBanco2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtBanco2ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(txtBanco2, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 160, 190, -1));
-        jPanel1.add(txtCuentas2, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 120, 190, -1));
 
         jLabel15.setText("Prov. Fantasía");
         jPanel1.add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(15, 292, -1, 14));
@@ -184,10 +133,28 @@ public class Pnl_3 extends javax.swing.JPanel {
         txaObservacionesDemanda.setRows(5);
         scpObservacionesDemanda.setViewportView(txaObservacionesDemanda);
 
-        jPanel1.add(scpObservacionesDemanda, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 60, 250, -1));
+        jPanel1.add(scpObservacionesDemanda, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 230, 230, -1));
 
         jLabel43.setText("Observación");
-        jPanel1.add(jLabel43, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 40, -1, -1));
+        jPanel1.add(jLabel43, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 240, -1, -1));
+
+        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null}
+            },
+            new String [] {
+                "Banco", "Cuenta"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable2);
+
+        jPanel1.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 30, 450, 170));
+
+        jLabel7.setText("Número contratación");
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(15, 39, -1, -1));
 
         add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 6, 967, 415));
     }// </editor-fold>//GEN-END:initComponents
@@ -197,7 +164,7 @@ public class Pnl_3 extends javax.swing.JPanel {
     }//GEN-LAST:event_txtProveedorRealActionPerformed
 
     private void txtProveedorFanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtProveedorFanActionPerformed
-        // TODO add your handling code here:
+
     }//GEN-LAST:event_txtProveedorFanActionPerformed
 
     private void txtBanco3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtBanco3ActionPerformed
@@ -209,7 +176,7 @@ public class Pnl_3 extends javax.swing.JPanel {
     }//GEN-LAST:event_txtBanco1ActionPerformed
 
     private void txtBanco2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtBanco2ActionPerformed
-        // TODO add your handling code here:
+
     }//GEN-LAST:event_txtBanco2ActionPerformed
 
 
@@ -222,10 +189,6 @@ public class Pnl_3 extends javax.swing.JPanel {
     private javax.swing.JComboBox<String> cmbOrdenDemanda;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
-    private javax.swing.JLabel jLabel11;
-    private javax.swing.JLabel jLabel12;
-    private javax.swing.JLabel jLabel13;
-    private javax.swing.JLabel jLabel14;
     private javax.swing.JLabel jLabel15;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -233,21 +196,16 @@ public class Pnl_3 extends javax.swing.JPanel {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel43;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTable2;
     private javax.swing.JScrollPane scpObservacionesDemanda;
     private javax.swing.JTextArea txaObservacionesDemanda;
-    private javax.swing.JTextField txtBanco1;
-    private javax.swing.JTextField txtBanco2;
-    private javax.swing.JTextField txtBanco3;
     private javax.swing.JTextField txtCedula;
     private javax.swing.JTextField txtCosto;
-    private javax.swing.JTextField txtCuentas1;
-    private javax.swing.JTextField txtCuentas2;
-    private javax.swing.JTextField txtCuentas3;
     private javax.swing.JTextField txtMontoOfertado;
     private javax.swing.JTextField txtNumeroContrato;
     private javax.swing.JTextField txtObjeto;
